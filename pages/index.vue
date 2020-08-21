e6a5ce03b72e20748f5846e3e6f53f4f6a5ea814
bagyga <ataraspost@gmail.com>
2020-08-21 10:21:09 +0700

    Логика для главной страницы

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
-    test
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="1">
+          <v-btn v-on:click="login" v-if="isLogin">Login</v-btn>
+          <v-btn v-on:clock="logout" v-else>Logout</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 
 export default {
-  components: {}
+  components: {
+  },
+  data () {
+    return {
+      isLogin: true
+    }
+  },
+  methods: {
+    login () {
+      this.$router.push('login-form')
+    },
+    logout () {
+      localStorage.removeItem('token')
+      this.isLogin = true
+    }
+  },
+  mounted () {
+    const token = localStorage.getItem('bgcolor')
+    if (token) {
+      this.isLogin = false
+    }
+  }
 }
 </script>
---
+++ pages/login-form.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      login Form
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
